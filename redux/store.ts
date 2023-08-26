e92cf2f8a608895031667ce1d63ac36d535b9078
duckduckcodes <0x07d2@proton.me>
2023-08-26 06:36:16 +0200

    initial commit

---
+++ redux/store.ts
@@ -0,0 +1,13 @@
+// // store.ts
+// import { configureStore } from "@reduxjs/toolkit";
+ 
+ 
+// const store = configureStore({
+//   reducer: {
+//     members: memberSlice,
+//    },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({ serializableCheck: false }),
+// });
+
+// export default store;
